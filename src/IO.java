672fcd578111358bc3795fdf816064ec82bbf58a
Duda <maria.oliveira@zup.com.br>
2024-08-27 12:27:59 -0300

    Class with menu of options

---
+++ src/IO.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class IO {
+
+    public static int menu (){
+        String builder = "Vamos ver como está a sua sorte hoje, puxe a alavanca para começar: (Enter + numero para selecionar)" +
+                "\nAperte 1 para puxar a alavanca no nivel fácil: " +
+                "\nAperte 2 para puxar a alavanca no nivel dificil se tiver coragem: " +
+                "\nAperte 3 para puxar a alavanca no nivel extremo se você é louco e compulsivo: ";
+        System.out.println(builder);
+        return new Scanner(System.in).nextInt();
+    }
+}
